dd6f62a0e9ea0dfd91e2a4170f60de6ba984cc89
Gabriel Tay <gabriel_tay@tech.gov.sg>
2022-07-13 15:38:19 +0800

    enable detekt

--- src/main/kotlin/io/tech/camunda/playground/delegate/HelloProcessDelegate.kt
+++ src/main/kotlin/io/tech/camunda/playground/delegate/HelloProcessDelegate.kt
@@ -1,12 +1,12 @@
 package io.tech.camunda.playground.delegate
 
 import mu.KotlinLogging
-import org.camunda.bpm.engine.delegate.DelegateExecution;
-import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.delegate.DelegateExecution
+import org.camunda.bpm.engine.delegate.JavaDelegate
 import org.springframework.stereotype.Component
 
 @Component
-class HelloProcessDelegate: JavaDelegate {
+class HelloProcessDelegate : JavaDelegate {
     private val logger = KotlinLogging.logger {}
 
     override fun execute(execution: DelegateExecution) {
