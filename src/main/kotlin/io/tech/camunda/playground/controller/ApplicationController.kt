3d8398346736adbfdf9ca600a65e87203215381d
Gabriel Tay <gabriel_tay@tech.gov.sg>
2022-07-13 14:14:56 +0800

    Working Camunda with Spring boot

--- src/main/kotlin/io/tech/camunda/playground/controller/ApplicationController.kt
+++ src/main/kotlin/io/tech/camunda/playground/controller/ApplicationController.kt
@@ -1,5 +1,9 @@
 package io.tech.camunda.playground.controller
 
+import org.camunda.bpm.engine.RuntimeService
+import org.joda.time.DateTime
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -7,8 +11,18 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api")
 class ApplicationController {
+    @Autowired
+    private lateinit var runtimeService: RuntimeService
+
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
     @GetMapping("/")
-    fun hello(): String {
-        return "hello world"
+    fun requestHello(): String {
+        logger.info("Hello world... start processing")
+        runtimeService.startProcessInstanceByKey(
+            "hello-process",
+            DateTime.now().toString(),
+        )
+        return "Hello World..."
     }
 }
---
+++ src/main/kotlin/io/tech/camunda/playground/delegate/HelloProcessDelegate.kt
@@ -0,0 +1,15 @@
+package io.tech.camunda.playground.delegate
+
+import mu.KotlinLogging
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component
+
+@Component
+class HelloProcessDelegate: JavaDelegate {
+    private val logger = KotlinLogging.logger {}
+
+    override fun execute(execution: DelegateExecution) {
+        logger.info { "inside hello processing" }
+    }
+}
